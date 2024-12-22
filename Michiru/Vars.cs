4ad8b242f13fd7d3d1f8ee4e04ed8481fec9bd55
Lily <admin@mintlily.lgbt>
2024-12-22 18:05:43 -0500

    version bump

--- Michiru/Vars.cs
+++ Michiru/Vars.cs
@@ -6,12 +6,13 @@ namespace Michiru;
 public static class Vars {
     public static string DNetVer => Assembly.GetAssembly(typeof(DiscordSocketClient))!.GetName().Version!.ToString(3);
     public const string Name = "Michiru";
-    private static readonly Version VersionObj = new (1, 10, 0);
+    private static readonly Version VersionObj = new (1, 12, 0);
     // public const ulong ClientId = 477202627285876756;
-    public const int TargetConfigVersion = 8;
+    public const int TargetConfigVersion = 9;
+    public const int TargetModConfigVersion = 1;
 
     public static readonly string VersionStr = VersionObj.ToString(3) + (IsDebug ? "-dev" : "");
-    public static readonly DateTime BuildTime = IsDebug ? DateTime.UtcNow : new DateTime(2024, 9, 26, ( 17 + 4 ), 48, 00);
+    public static readonly DateTime BuildTime = IsDebug ? DateTime.UtcNow : new DateTime(2024, 12, 22, ( 17 + 5 ), 41, 00);
     public const bool IsDebug = false;
     public static string BuildDate => $"{BuildTime:F}";
     public static DateTime StartTime { get; set; }
@@ -19,5 +20,5 @@ public static class Vars {
     public const string SupportServer = "https://discord.gg/Qg9eVB34sq";
     public const ulong SupportServerId = 1083619886980403272;
     // public static readonly string BotUserAgent = $"Mozilla/5.0 {(IsWindows ? "(Windows NT 10.0; Win64; x64; rv:115.0)" : "(X11; Linux x86_64)")} (compatible; {Name}/{VersionObj.ToString(3)}; +https://discordapp.com)";
-    public static string DotNetTargetVersion => "8.0.8";
+    public static string DotNetTargetVersion => "8.0.11";
 }
