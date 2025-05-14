2a0aba2e678be11cff070178aa93126d072be61c
Lily C. <admin@mintlily.lgbt>
2025-05-14 19:46:29 -0400

    version bump

--- Michiru/Vars.cs
+++ Michiru/Vars.cs
@@ -6,9 +6,9 @@ namespace Michiru;
 public static class Vars {
     public static string DNetVer => Assembly.GetAssembly(typeof(DiscordSocketClient))!.GetName().Version!.ToString(3);
     public const string Name = "Michiru";
-    private static readonly Version VersionObj = new (1, 12, 10);
+    private static readonly Version VersionObj = new (1, 12, 11);
     // public const ulong ClientId = 477202627285876756;
-    public const int TargetConfigVersion = 9;
+    public const int TargetConfigVersion = 10;
     public const int TargetModConfigVersion = 1;
     public const int TargetMusicConfigVersion = 1;
 
@@ -21,5 +21,5 @@ public static class Vars {
     public const string SupportServer = "https://discord.gg/Qg9eVB34sq";
     public const ulong SupportServerId = 1083619886980403272;
     // public static readonly string BotUserAgent = $"Mozilla/5.0 {(IsWindows ? "(Windows NT 10.0; Win64; x64; rv:115.0)" : "(X11; Linux x86_64)")} (compatible; {Name}/{VersionObj.ToString(3)}; +https://discordapp.com)";
-    public static string DotNetTargetVersion => "9.0.3";
+    public static string DotNetTargetVersion => "9.0.5";
 }
